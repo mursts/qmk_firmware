9ebcd15fcbd3bed9b9723ebc69586483060bb542
mursts <mursts@gmail.com>
2022-05-18 21:26:37 +0900

    キーマップを整理

--- keyboards/mursts/tofu40/keymaps/default/keymap.c
+++ keyboards/mursts/tofu40/keymaps/default/keymap.c
@@ -15,51 +15,48 @@
  */
 #include QMK_KEYBOARD_H
 
-// Defines names for use in layer keycodes and the keymap
-enum layer_numbers {
-    _BASE = 0,
-    _LOWER,
-    _RAISE,
-    _ADJUST,
-    _FUNCTION,
-};
+/* enum custom_keycodes { */
+/*     M_HNKN */
+/* }; */
 
-#define KC_CTTB  LCTL_T(KC_TAB)
-#define KC_SFES  LSFT_T(KC_ESC)
-#define KC_V_RS  LT(_RAISE, KC_V)
-#define KC_B_AD  LT(_ADJUST, KC_N)
-#define KC_EN_FN LT(_FUNCTION, KC_ENT)
+#define LT_CTAB  LCTL_T(KC_TAB)
+#define LT_SESC  LSFT_T(KC_ESC)
+#define LT_CESC  LCTL_T(KC_ESC)
+#define LT_1SPC  LT(1, KC_SPC)
+#define LT_2V    LT(2, KC_V)
+#define LT_3N    LT(3, KC_N)
+#define LT_4ENT  LT(4, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT(
-        KC_ESC,  KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
-        KC_CTTB, KC_A,   KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_EN_FN,
-        KC_SFES, KC_Z,   KC_X,     KC_C,    KC_V_RS, KC_B,    KC_B_AD, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LCTL, KC_LGUI,KC_LALT,  KC_SPC,  _______, _______, _______, _______, _______
+    [0] = LAYOUT(
+        KC_ESC,  KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        LT_CTAB, KC_A,   KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT_4ENT,
+        LT_SESC, KC_Z,   KC_X,     KC_C,    LT_2V,   KC_B,    LT_3N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+        LT_CESC, KC_LGUI,KC_LALT,  KC_SPC,  _______, LT_1SPC, _______, _______, _______
         ),
 
-    [_LOWER] = LAYOUT(
+    [1] = LAYOUT(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
 		_______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______
 		),
 
-    [_RAISE] = LAYOUT(
+    [2] = LAYOUT(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
 		_______, KC_LCBR, KC_RCBR, _______, KC_PIPE, _______, KC_UNDS, KC_PLUS, KC_COLN, KC_DQUO, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______
 		),
 
-    [_ADJUST] = LAYOUT(
+    [3] = LAYOUT(
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, KC_LBRC, KC_RBRC, _______, KC_BSLS, _______, KC_MINS, KC_EQL,  KC_SCLN, KC_QUOT, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______
 		),
 
-    [_FUNCTION] = LAYOUT(
+    [4] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_DEL,
 		_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______, _______, _______, _______, _______,
 		_______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______,
