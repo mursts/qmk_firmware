a7ceed620484bf1f52fcb43ee533c32e4cfd6f1e
mursts <mursts@gmail.com>
2022-08-05 17:27:14 +0900

    add new my keyboard bellis40

---
+++ keyboards/mursts/bellis40/rules.mk
@@ -0,0 +1,16 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+ENCODER_ENABLE = no
+
+VIA_ENABLE = yes
